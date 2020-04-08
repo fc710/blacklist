82b0d5891cdb33364f0da6836956501708bf7455
fc710 <fabiocasiraghi3@gmail.com>
2020-04-08 14:21:20 +0200

    fixing formatting

--- check_score.sh
+++ check_score.sh
@@ -14,9 +14,9 @@ re='^\+?[0-9]+$'
 
 #not supported in ash
 if ! [[ $caller_num =~ $re ]]; then
-    #echo "Exiting: Nan"
-    echo 'HANGUP'
-    exit 1
+	#echo "Exiting: Nan"
+	echo 'HANGUP'
+	exit 1
 fi
 #echo "Calling number read in script : $caller_num" 
 score_page=$(curl -sm 3 https://www.tellows.it/num/"$caller_num")
@@ -29,8 +29,8 @@ score=${score: -2:1}
 #score=${score:$pos:1}
 #echo "Score parsed online: $score"
 if [[ $score -gt 5 && $score -lt 10 ]]; then
-    echo 'SET VARIABLE is_annoying "1" '; else
-    echo 'SET VARIABLE is_annoying "0" '
+	echo 'SET VARIABLE is_annoying "1" '; else
+	echo 'SET VARIABLE is_annoying "0" '
 fi
 
 exit 0
